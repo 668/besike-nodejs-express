b8fc3e0ec40fc2bf9c82540fd3392d54281824f4
Hanfei <xuhanf@gmail.com>
2015-05-21 10:53:11 +0200

    Rendering Less Templates

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ module.exports = createMiniHarp;
 
 var connect =  require('connect');
 var makeJade =  require('./lib/processor/jade');
+var makeLess = require('./lib/processor/less');
 
 function createMiniHarp(root) {
     var app = connect();
 
     app.use(makeJade(root));
+    app.use(makeLess(root));
 
     return app;
 }
